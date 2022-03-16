1f07a3d51f832b660bea9de9a2e012c3e3bc272c
Michaela Wilson <+>
2022-03-15 19:32:12 -0600

    commit tree 3f4ada90a792b36399ca9741c638ad921b9f693b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['Guadalupa', 'Ollie', 'Aki'];
+const event = 'surprise';
+
+function writeCards(names, suprise){
+    let newArray = []
+    for (let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+
+function countDown(positiveNumber){
+    while (positiveNumber >= 0){
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
